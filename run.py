4cf7ec3119ae2679039dd1bffc137e9abeb29407
rebazp <>
2023-10-27 13:24:19 +0000

    Fixed syntax error in display score section.

--- run.py
+++ run.py
@@ -1,3 +1,104 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+Function for new game
+"""
+def new_game():
+    guesses = []
+    correct_guesses = 0
+    question_number = 1
+
+    for key in questions:
+        print("-------------------------")
+        print(key)
+        for i in options[question_num-1]:
+            print(i)
+        guess = input("Enter (A, B, C, D): ")
+        guess = guess.upper()
+        guesses.append(guess)
+
+        correct_guesses += check_answer(questions.get(key), guess)
+        question_num += 1
+
+    display_score(correct_guesses, guesses)
+
+"""
+Function to check the answers
+"""
+def check_answer(answer, guess):
+
+    if answer == guess:
+        print("Correct answer!")
+        return 1
+    else:
+        print ("Wrong answer!")
+        return 0
+
+"""
+Function to display the score
+"""
+def display_score(correct_guesses, guesses):
+    print ("-------------------------")
+    print ("Results")
+    print ("-------------------------")
+    
+    print ("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end="")
+    print()
+
+     print ("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score = int((correct_guesses/len(questions))*100)
+    print("Your Score is: "+str(score)+"%")
+
+"""
+Function to replay the game
+"""
+def play_again():
+    response = input("Plaay again? (Yes or No): ")
+    response = response.upper()
+
+    if response == "YES":
+        return True
+    else:
+        return False
+
+
+"""
+Collection for the questions
+"""
+questions = {
+"Which is the biggest planet in our solar system?: ": "A",
+"Which is the biggest mamal on planet earth?: ": "B",
+"Which is the fastes animal on planet earth?: ": "A",
+"Who wrote the epic stories of Harry Potter?: ": "C",
+"Which wrote the famous book The Alchemist?: ": "D",
+"What's The Batmans real name?: ": "D",
+"What's the name of the director to The Gladiator starring Russell Crowe?: ": "A",
+"How many letters are there in the English aplhabet?: ": "B",
+"Who made the Mona Lisa painting?: ": "A",
+"How many bones does the human hand have?: ": "A",
+}
+
+"""
+Collection for the options
+"""
+options = [["A. Jupiter", "B. Uranus", "C. Saturn", "D. Neptune"],
+["A. Elephant", "B. Blue Whale", "C. Gray Whale", "D. Clown Fish"],
+["A. Peregrine Falcon", "B. Cheetah", "C. Black Marlin", "D. Turtle"],
+["A. J.R.R. Tolkien", "B. Stephen King", "C. J.K. Rowling", "D. George Orwell"],
+["A. Ian Fleming", "B. William Shakespeare", "C. C.S. Lewis", "D. Paulo Coelho"],
+["A. Clark Kent", "B. Peter Parker", "C. Chuck Norris", "D. Bruce Wayne"],
+["A. Ridley Scott", "B. Martin Scorsese", "C. Christopher Nolan", "D. Guillermo del Toro"],
+["A. 25", "B. 26", "C. 27", "D. 28"],
+["A. Vincent van Gogh", "B. Leonardo da Vinci", "C. Pablo Picasso", "D. Claude Monet"],
+["A. 27", "B. 26", "C. 25", "D. 30"]]
+
+new game()
+
+while play_again():
+    new_game()
+
+print("SEE YOU SOON")
